55c76764f25a35d7698d7de6d55764b86e7c23a4
Zach Zeli <zelizach@gmail.com>
2022-01-12 14:52:00 -0500

    Updated api for LWC product card

--- exp-pkg/main/default/lwc/productCard/productCard.js
+++ exp-pkg/main/default/lwc/productCard/productCard.js
@@ -15,80 +15,54 @@ export default class ProductCard extends LightningElement {
         alt: 'Generic Product Image'
     };
     
-    // Helper to retrieve input data
-    productField(property) {
-        // if (!this.productData || !this.productData.hasOwnProperty(property)) { return null; }
-        // return this.productData[property];
-        return this.dataParser("product", property);
-    }
-    orderField(property) {
-        // if (!this.orderData || !this.orderData.hasOwnProperty(property)) { return null; }
-        // return this.orderData[property];
-        return this.dataParser("order", property);
-    }
-
     @api
-    itemData;
-    dataParser(type, property) {
+    itemData = {
+        Product2: {
+            Name: 'Ficus',
+            ProductCode: 'P000MRB',
+            HasVarieties__c: true,
+            SubVariety__c: 'Burgundy and dark Green',
+            Size__c: 'Medium',
+            Difficulty__c: 'Regular',
+            Light_Level__c: 'Bright',
+            IsPetFriendly__c: false,
+            HasColor__c: true,
+            HasFlowers__c: false,
+        },
+        UnitPrice: 25.03,
+        Quantity: 7,
+    };
+    dataParser(property) {
         if (!this.itemData) { return null; }
-        switch (type) {
-            case "product":
-                if (!this.itemData.hasOwnProperty("Product2")) { return null; }
-                if (!this.itemData.Product2.hasOwnProperty(property)) { return null; }
-                return this.itemData.Product2[property];
-            case "order":
-                if (!this.itemData.hasOwnProperty(property)) { return null; }
-                return this.itemData[property];
-            default:
-                console.log("Unkown data type: " + type);
+
+        if (property === "UnitPrice" || property === "Quantity" ) {
+            if (!this.itemData.hasOwnProperty(property)) { return null; }
+            return this.itemData.property;
         }
+
+        if (!this.itemData.hasOwnProperty("Product2")) { return null; }
+        if (!this.itemData.Product2.hasOwnProperty(property)) { return null; }
+        return this.itemData.Product2.property;
     }
-
-    // // Object of 'Product2' type: retrieve attributes from 'productField(prop)' method
-    // @api
-    // productData;
-    // // Object of 'OrderItem' type: retrieve attributes from 'productField(prop)' method
-    // @api
-    // orderData;
-
-    // // MOCK DATA
-    // productData = {
-    //     name: 'Ficus',
-    //     ProductCode: 'P000MRB',
-    //     HasVarieties__c: true,
-    //     SubVariety__c: 'Burgundy and dark Green',
-    //     Size__c: 'Medium',
-    //     Difficulty__c: 'Regular',
-    //     Light_Level__c: 'Bright',
-    //     IsPetFriendly__c: false,
-    //     HasColor__c: true,
-    //     HasFlowers__c: false,
-    // };
-    // orderData = {
-    //     Product2Id: 'mockProduct2Id',
-    //     PricebookEntryId: 'mockPrickbookEntryId',
-    //     Quantity: 3,
-    //     UnitPrice: 23.00,
-    // };
 
     // Data Bindings
     get fullName() {
-        let name = this.productField('Name') || 'Basic Product';
-        if (this.productField('HasVarieties__c')) {
-            name += ': ' + this.productField('SubVariety__c');
+        let name = this.dataParser('Name') || 'Basic Product';
+        if (this.dataParser('HasVarieties__c')) {
+            name += ': ' + this.dataParser('SubVariety__c');
         }
         return name;
     }
     get itemName() {
-        return this.productField('Name') || 'Basic Product';
+        return this.dataParser('Name') || 'Basic Product';
     }
     get variantName() {
-        return this.productField('SubVariety__c') || '';
+        return this.dataParser('SubVariety__c') || '';
     }
     get unitPrice() {
         let price = 'Not Available';
-        if (this.orderField('UnitPrice')) {
-            price = '$' + this.orderField('UnitPrice');
+        if (this.dataParser('UnitPrice')) {
+            price = '$' + this.dataParser('UnitPrice');
             if (this.isCartItem) {
                 price += ' each';
             }
@@ -96,11 +70,11 @@ export default class ProductCard extends LightningElement {
         return price;
     }
     get cartQuantity() {
-        return this.orderField('Quantity') || 0;
+        return this.dataParser('Quantity') || 0;
     }
     get cartTotal() {
-        let q = this.orderField('Quantity');
-        let p = this.orderField('UnitPrice');
+        let q = this.dataParser('Quantity');
+        let p = this.dataParser('UnitPrice');
         if (p && q) {
             return 'Subtotal: $' + (p*q);
         }
