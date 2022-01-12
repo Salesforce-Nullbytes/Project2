be06ef6061e95b6b252907aa6ebb27b98186f34a
Zach Zeli <zelizach@gmail.com>
2022-01-12 13:17:19 -0500

    Hooks into LWC from Aura for forum

--- exp-pkg/main/default/aura/forumHolder/forumHolderHelper.js
+++ exp-pkg/main/default/aura/forumHolder/forumHolderHelper.js
@@ -4,7 +4,9 @@
 
         apexMethod.setCallback(this, function (response) {
             if (response.getState() == 'SUCCESS') {
-                component.set("v.topLevelPosts", response.getReturnValue());
+                let posts = response.getReturnValue();
+                component.set("v.topLevelPosts", posts);
+                component.set("v.topList", this.BuildTopList(posts));
             } else {
                 console.log("Error retrieving top level posts!");
             }
@@ -12,9 +14,22 @@
 
         $A.enqueueAction(apexMethod);
     }, 
-    ItemChosen: function(component, index) {
-        this.SetParentPostFromTop(component, index);
-        this.PrintTest();
+    SetParentPostFromTop: function(component, index) {
+        let post = null;
+        if (component.get("v.showTop")) {
+            post = component.get("v.topLevelPosts")[index];
+        } else {
+            post = component.get("v.childPosts")[index];
+        }
+        if (post == null) {
+            console.log("ERROR: post chosen does not have valid index!");
+            return;
+        }
+
+        component.set("v.selectedPost", post);
+        component.set("v.selectedId", post.Id);
+        component.set("v.hasSelection", true);
+        console.log("SELECTED ID IS " + post.Id);
     },
     ApexSetValueChildPosts: function(component, helper, fromId) {
         let apexMethod = component.get("c.GetPostsUnder");
@@ -37,118 +52,96 @@
         }
 
         component.set("v.childPosts", children);
-        console.log("Set # children: " + component.get("v.childPosts").length);
+        //console.log("Set # children: " + component.get("v.childPosts").length);
         
         component.set("v.showTop", false);
         this.ResetSelection(component);
         this.BuildDisplayTree(component);
-    },
-    SetParentPostFromTop: function(component, index) {
-        let post = null;
-        if (component.get("v.showTop")) {
-            post = component.get("v.topLevelPosts")[index];
-        } else {
-            post = component.get("v.childPosts")[index];
-        }
-        if (post == null) {
-            console.log("ERROR: post chosen does not have valid index!");
-            return;
-        }
-
-        component.set("v.selectedPost", post);
-        component.set("v.selectedId", post.Id);
-        component.set("v.hasSelection", true);
-        console.log("SELECTED ID IS " + post.Id);
     },
     ResetSelection: function(component) {
         component.set("v.selectedPost", null);
         component.set("v.selectedId", null);
         component.set("v.hasSelection", false);
     },
-    PrintTest: function() {
-        console.log("Hello!");
-    },
     BuildDisplayTree: function(component) {   
         let parent = component.get("v.parentPost");
-        let parentDepth = parent.Depth__c;
-
         let allPosts = component.get("v.childPosts");
-        let topLevel = [];
-        let secondLevel = [];
+
+        let parentDepth = parent.Depth__c;
+        let postsByDepth = [];
+
+        postsByDepth[0] = [];
+        postsByDepth[0].push(this.MakeNode(parent));
 
         for (let post of allPosts) {
-            switch (post.Depth__c - parentDepth) {
-                case 1:
-                    if (post.Parent__c == parent.Id)
-                        topLevel.push(post);
-                    break;
-                case 2:
-                    secondLevel.push(post);
-                    break;
+            let depth = post.Depth__c - parentDepth;
+            if (depth < 0) {continue;} // Review this condition later
+            
+            if (postsByDepth[depth] === undefined) postsByDepth[depth] = [];
+            postsByDepth[depth].push(this.MakeNode(post));
+        }
+
+
+        // Traverse from bottom to top of tree.
+        // Start one before the last depth.
+        // Iterate posts at depth beneath it.
+        //   Iterate posts at that depth
+        //   If child parent is the current post, push to children
+        for (let i = postsByDepth.length-2; i >= 0; i--) {
+            for (let comment of postsByDepth[i+1]) {
+                for (let post of postsByDepth[i]) {
+                    this.PushIfChild(post, comment);
+                }   
             }
         }
 
-        let tree = {};
-        let tree2 = [];
-        for (let i = 0; i < topLevel.length; i++) {
-            let comment = topLevel[i];
-            tree[comment.Id] = {};
-            tree[comment.Id].comment = comment;
-
-            let entry = {};
-            entry.comment = comment;
-            entry.subcomments = [];
-
-            let subLevel = [];
-            for (let subcomment of secondLevel) {
-                if (subcomment.Parent__c == comment.Id) {
-                    subLevel.push(subcomment);
-                    entry.subcomments.push(subcomment);
-                }
-            }
-            tree[comment.Id].subcomments = subLevel;
-            tree2.push(entry);
-
-            // let subArray = [];
-            // subArray.push(topLevel[i]);
-            // for (let comment of secondLevel) {
-            //     if (comment.Parent__c.Id == topLevel[i].Id) {
-            //         subArray.push(comment);
-            //     }
-            // }
-            // output.push(subArray);
-        }
-
-        //this.PrintTree(tree);
-        component.set("v.displayTree", tree);
-        component.set("v.displayTree2", tree2);
+        //this.PrintTree(postsByDepth[0][0], 0);
+        component.set("v.displayTree", postsByDepth[0][0]);
     },
-    PrintTree: function(tree) {
-        console.log("PRINTING TREE FUNCTION!");
-        for (let id in tree) {
-            console.log("Comment: " + tree[id].comment.Id);
-            for (let subcomment of tree[id].subcomments) {
-                console.log("   Sub: " + subcomment.Id);
-            }
+    PrintTree: function(node, depth) {
+        let indent = ""; 
+        for (let i=0;i<depth;i++) indent += "  ";
+
+        if (!node) {
+            console.log(indent+"node does not exist...");
+            return;
+        }
+        for (let key in node.self) {
+            console.log(indent+"self." + key + ": " +node.self[key]);
+        }
+        console.log(indent+"children:");
+        for (let child of node.children) {
+            this.PrintTree(child, depth+1);
         }
     },
-    // IterateBranch: function(tree, level, subId) {
-    //     if (level == 2 && subId == null) {
-    //         console.log("Require subId on level 2 query!");
-    //         return [];
-    //     }
-    //     if (level == 2) {
-    //         return tree[subId].subcomments;
-    //     }
-    //     if (level == 1) {
-    //         let result = [];
-    //         for (let key in tree) {
-    //             result.push(tree[key].comment);
-    //         }
-    //         return result;
-    //     }
+    MakeNode: function(post) {
+        return {
+            self: post,
+            children: [],
+        };
+    },
+    PushIfChild: function(node, child) {
+        if (!node || !node.hasOwnProperty("self") || !node.hasOwnProperty("children")) {
+            console.log("Invalid node! Requires a self and children.");
+            return;
+        }
 
-    //     console.log("Can only iterate levels 1-2");
-    //     return;
-    // },
+        if (!child) { return; }
+        if (!child.hasOwnProperty("self") || !child.hasOwnProperty("children")) {
+            console.log("Invalid child! Requires a self and children.");
+            return;
+        }
+
+        if (child.self.Parent__c == node.self.Id) {
+            node.children.push(child);
+        }
+    },
+    BuildTopList: function(posts) {
+        let list = [];
+        for (let post of posts) {
+            list.push(this.MakeNode(post));
+        }
+        return list;
+    },
 })
+
