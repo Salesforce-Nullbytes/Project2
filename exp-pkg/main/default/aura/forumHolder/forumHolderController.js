12d8f46e2349499ad615ee03117f2a51f195039b
Zach Zeli <zelizach@gmail.com>
2022-01-11 20:45:56 -0500

    basic functionality in Aura Forum component

--- exp-pkg/main/default/aura/forumHolder/forumHolderController.js
+++ exp-pkg/main/default/aura/forumHolder/forumHolderController.js
@@ -1,15 +1,30 @@
 ({
-    SetTopLevelPosts : function(component, event, helper) {
-        let apexMethod = component.get("c.GetTopLevelPosts");
-
-        apexMethod.setCallback(this, function (response) {
-            if (response.getState() == 'SUCCESS') {
-                component.set("v.topLevelPosts", response.getReturnValue());
-            } else {
-                console.log("Error retrieving top level posts!");
-            }
-        });
-
-        $A.enqueueAction(apexMethod);
+    Initialize : function(component, event, helper) {
+        helper.ApexSetTopLevelPosts(component);
     }, 
+    HandleChoose: function(component, event, helper) {
+        let index = event.getParam('id');
+        console.log('index: ' + index);
+
+        helper.ItemChosen(component, index);
+    },
+    OpenSelected: function(component, event, helper) {
+        // Set parent post
+        component.set("v.parentPost", component.get("v.selectedPost"));
+        // Set children
+        let parentId = component.get("v.selectedId");
+        console.log("parent id is " + parentId);
+
+        helper.ApexSetValueChildPosts(component, helper, parentId);
+    },
+    HandleTop: function(component, event, helper) {
+        component.set("v.showTop", true);
+    },
+    PrintChildren: function(component, event, helper) {
+        let array = component.get("v.childPosts");
+        console.log("Printing children...");
+        for (let child of array) {
+            console.log("    child found: " + array);
+        }
+    },
 })
