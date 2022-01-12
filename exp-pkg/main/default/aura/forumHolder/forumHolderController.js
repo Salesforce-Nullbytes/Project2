5318d1600551b5df64d6eecabff89c29db0a8e0d
Zach Zeli <zelizach@gmail.com>
2022-01-12 11:09:16 -0500

    Modifications of forum holder

--- exp-pkg/main/default/aura/forumHolder/forumHolderController.js
+++ exp-pkg/main/default/aura/forumHolder/forumHolderController.js
@@ -4,8 +4,6 @@
     }, 
     HandleChoose: function(component, event, helper) {
         let index = event.getParam('id');
-        console.log('index: ' + index);
-
         helper.ItemChosen(component, index);
     },
     OpenSelected: function(component, event, helper) {
@@ -20,11 +18,14 @@
     HandleTop: function(component, event, helper) {
         component.set("v.showTop", true);
     },
-    PrintChildren: function(component, event, helper) {
-        let array = component.get("v.childPosts");
-        console.log("Printing children...");
-        for (let child of array) {
-            console.log("    child found: " + array);
-        }
-    },
+    // TreeKeys: function(component, event, helper) {
+    //     console.log("Calling tree keys");
+    //     let obj = component.get("v.displayTree");
+    //     let idList = [];
+    //     for (let key in obj) {
+    //         idList.push(obj[key]);
+    //         console.log("pushed key " + key);
+    //     }
+    //     return idList;
+    // },
 })
