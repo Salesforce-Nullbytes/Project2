53e3f0870c4a89c53204d367b289dcc3656684b5
Rebecca <rebeccahahn40@gmail.com>
2022-01-14 08:28:50 -0500

    Styling updates for header, homepagequizicons, and forum page

--- exp-pkg/main/default/lwc/header/header.js
+++ exp-pkg/main/default/lwc/header/header.js
@@ -9,7 +9,16 @@ export default class header extends LightningElement {
 
     handleClick(event) {
         this.value = event.target.innerText;
-        console.log("this.value: " + this.value);
+
+        let valueChangeEvent = new CustomEvent("valuechange", {
+            detail: { value : this.value }
+        });
+
+        this.dispatchEvent(valueChangeEvent);
+    }
+
+    handleClickImage(event) {
+        this.value = "Cart";
 
         let valueChangeEvent = new CustomEvent("valuechange", {
             detail: { value : this.value }
