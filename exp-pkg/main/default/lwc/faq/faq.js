c12b5bec6e611c152e14fa5e57a9682ac70c76e8
Rebecca <rebeccahahn40@gmail.com>
2022-01-16 22:19:03 -0500

    New FAQ page

---
+++ exp-pkg/main/default/lwc/faq/faq.js
@@ -0,0 +1,20 @@
+import {LightningElement} from 'lwc';
+import confirmationBackgroundImage from '@salesforce/resourceUrl/confirmationBackgroundImage';
+
+export default class cartEmpty extends LightningElement {
+
+    // backgroundRendered = false;
+
+    // renderedCallback() {
+    //     if (this.backgroundRendered) {
+    //         return;
+    //     } 
+
+    //     let container = this.template.querySelectorAll(".background-image-container");
+    //     container[0].style.backgroundImage = `url(${confirmationBackgroundImage})`;
+    //     container[1].style.backgroundImage = `url(${confirmationBackgroundImage})`;
+
+    //     this.backgroundRendered = true;
+    // }
+
+}
